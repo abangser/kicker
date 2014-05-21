6c0e6beab85150c360a5277462469abbd5bc877f
abangser <abangser@thoughtworks.com>
2014-05-20 22:34:42 -0400

    23 - basic servlet hello world

---
+++ src/main/java/com/abangser/kicker/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+package main.java.com.abangser.kicker;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getOutputStream().write("Kicker, coming soon to an enterprise stack near you!".getBytes());
+    }
+}
